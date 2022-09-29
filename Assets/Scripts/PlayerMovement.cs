f924d883288db7517397bc6905bd0b8c5a525b5f
Artun Cimensel <49254881+ACimensel@users.noreply.github.com>
2022-09-29 14:39:40 -0700

    Set up the attack animation and fixed bugs with the animator parameters

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,25 +9,31 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float runSpeed = 40f;
 
-	float horizontalMove = 0f;
-	bool jump = false;
-	bool crouch = false;
+	private AnimatorClipInfo[] animCurrentClipInfo;
+	private float horizontalMove = 0f;
+	private float prevHorizontalMove = 0f;
+	private bool jump = false;
+	private bool crouch = false;
 	
 	// Update is called once per frame
 	void Update(){
-		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		ControlSpeed();
 		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-		if (Input.GetButtonDown("Jump")){
+		if (Input.GetButtonDown("Jump") && !animator.GetBool("Crouching")){
 			jump = true;
 			animator.SetBool("Jumping", true);
 		}
 
-		if (Input.GetButtonDown("Crouch")){
+		if (Input.GetButton("Crouch") && !animator.GetBool("Jumping")){
 			crouch = true;
 		} 
 		else if (Input.GetButtonUp("Crouch")){
 			crouch = false;
+		}
+		
+		if (Input.GetButtonDown("Attack") && !animator.GetBool("Crouching")){
+			animator.SetTrigger("Attack");
 		}
 	}
 
@@ -35,6 +41,18 @@ public class PlayerMovement : MonoBehaviour {
 		// Move our character
 		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
 		jump = false;
+	}
+
+	void ControlSpeed(){
+		animCurrentClipInfo = animator.GetCurrentAnimatorClipInfo(0);
+		if(animCurrentClipInfo[0].clip.name == "FemWarrior_attack"){
+			horizontalMove = (animator.GetBool("Jumping")) ? prevHorizontalMove : 0f;
+			// horizontalMove = 0f;
+		}
+		else{
+			prevHorizontalMove = horizontalMove;
+			horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		}
 	}
 
 	public void OnLanding(){
