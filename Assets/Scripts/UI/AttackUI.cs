4269991ff384b4a5fa05a33a863d81735ded32f6
Pouria Delfanazari <pdelfan@gmail.com>
2022-10-28 15:13:53 -0700

    Added attack ui (melee and placeholder for ranged)

---
+++ Assets/Scripts/UI/AttackUI.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttackUI : MonoBehaviour
+{
+
+    public Sprite meleeImg0;
+    public Sprite meleeImg1;
+    public Sprite meleeImg2;
+    public Sprite meleeImg3;
+    public Sprite rangedImg0;
+
+    private Image rangedIcon;
+    private Image meleePlaceholder;
+    private Image rangedPlaceholder;
+    private AnimatorClipInfo[] guardianAnimator;
+
+    private float meele1Time = 12.0f;
+
+    private string currentAnimation;
+
+
+    void Awake()
+    {
+        meleePlaceholder = GameObject.Find("MeleeImg").GetComponent<Image>();   
+        rangedPlaceholder = GameObject.Find("RangedImg").GetComponent<Image>();        
+     
+    }
+
+    void Start()
+    {
+        meleePlaceholder.sprite = meleeImg1;
+        rangedPlaceholder.sprite = rangedImg0; 
+    }
+
+    void Update()
+    {
+        guardianAnimator = GameObject.Find("Guardian").GetComponent<Animator>().GetCurrentAnimatorClipInfo(0);
+        currentAnimation = guardianAnimator[0].clip.name;
+        UpdateMeleeImg();
+    }
+
+
+
+
+    void ChangeImg(Image placeholder, Sprite img)
+    {
+        placeholder.sprite = img;
+    }
+
+    void UpdateMeleeImg()
+    {
+        switch (currentAnimation)
+        {
+            case "Guardian_melee1":
+                ChangeImg(meleePlaceholder, meleeImg1);
+                break;
+            case "Guardian_melee2":
+                ChangeImg(meleePlaceholder, meleeImg2);
+                break;
+            case "Guardian_melee3":
+                ChangeImg(meleePlaceholder, meleeImg3);
+                break;
+            default:
+                ChangeImg(meleePlaceholder, meleeImg0);
+                break;
+        }
+    }
+
+    void UpdateRangedImg() {
+
+    }
+
+
+}
