f4de4353d91918258607066bd9f6d540626d64b6
Pouria Delfanazari <pdelfan@gmail.com>
2022-10-29 16:25:02 -0700

    Added shield UI update

--- Assets/Scripts/ShieldSkill/Shield.cs
+++ Assets/Scripts/ShieldSkill/Shield.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-// u to enable -
-// 3 hits to break 
-// lasts 12s -
-// 5s cooldown
 
 public class Shield : MonoBehaviour
 {
@@ -15,7 +13,11 @@ public class Shield : MonoBehaviour
     private bool isShieldActive;
     private bool isCoolingDown;
     private Animator animator;
+
     [SerializeField] float shieldHealth;
+    private TMP_Text shieldTimer;
+    private Image shieldHolder;
+    private Image shieldImg;
 
 
     void Awake()
@@ -24,10 +26,14 @@ public class Shield : MonoBehaviour
         shield.SetActive(false);
         isShieldActive = false;
         timeLeft = 3f;
-        cooldownTime = 6f;
+        cooldownTime = 12f;
         isCoolingDown = false;
         animator = shield.GetComponent<Animator>();
         shieldHealth = 3f;
+        shieldTimer = GameObject.Find("ShieldTimer").GetComponent<TMP_Text>();
+        shieldTimer.text = "";
+        shieldHolder = GameObject.Find("ShieldHolder").GetComponent<Image>();
+        shieldImg = GameObject.Find("ShieldImg").GetComponent<Image>();
     }
 
     void Start()
@@ -37,6 +43,7 @@ public class Shield : MonoBehaviour
     void Update()
     {
         ManageShield();
+        UpdateShieldImg();
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -58,25 +65,44 @@ public class Shield : MonoBehaviour
         {
             animator.SetTrigger("shieldEnded");
             isShieldActive = false;
-            //shield.SetActive(false);
             timeLeft = 3f;
+            isCoolingDown = true;
         }
     }
 
-    void Cooldown()
+    void CooldownCountdown()
     {
+        shieldTimer.text = cooldownTime.ToString("F0");
+        Debug.Log("COOL: " + cooldownTime);
         if (cooldownTime > 0f)
         {
             cooldownTime -= Time.deltaTime;
+        } else {
+            isCoolingDown = false;
+        }
+    }
+
+    void UpdateShieldImg()
+    {
+        if (isShieldActive == false && isCoolingDown)
+        {
+            ChangeTextOpacity(shieldTimer, 1f);
+            CooldownCountdown();
+            ChangeImgOpacity(shieldImg, 0.2f);
+            ChangeImgOpacity(shieldHolder, 0.2f);
         }
         else
         {
+            ChangeImgOpacity(shieldImg, 1f);
+            ChangeImgOpacity(shieldHolder, 1f);
+            if (cooldownTime < 0) { cooldownTime = 12f; }
+            ChangeTextOpacity(shieldTimer, 0f);
         }
     }
 
     void ManageShield()
     {
-        if (Input.GetButtonDown("Skill_Shield") && !isShieldActive)
+        if (Input.GetButtonDown("Skill_Shield") && !isCoolingDown)
         {
             shield.SetActive(true);
             isShieldActive = true;
@@ -85,6 +111,21 @@ public class Shield : MonoBehaviour
         {
             ShieldCountdown();
         }
+    }
+
+
+    void ChangeImgOpacity(Image img, float opacity)
+    {
+        Color tmp = img.GetComponent<Image>().color;
+        tmp.a = opacity;
+        img.GetComponent<Image>().color = tmp;
+    }
+
+    void ChangeTextOpacity(TMP_Text tmpText, float opacity)
+    {
+        Color tmp = tmpText.GetComponent<TMP_Text>().color;
+        tmp.a = opacity;
+        tmpText.GetComponent<TMP_Text>().color = tmp;
     }
 
 
