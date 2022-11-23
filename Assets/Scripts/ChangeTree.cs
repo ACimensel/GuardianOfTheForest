c758ac0409991a8642b6b89b10368ea8ad0ac66d
ACimensel <cimensel.a@hotmail.com>
2022-11-22 18:46:40 -0800

    set up the hub tree animations, animator and script
    -Added tree sprites
    -created tree animations, animator, transitions
    -logic for what animation to play based on the PersistantData singleton class values

--- Assets/Scripts/ChangeTree.cs
+++ Assets/Scripts/ChangeTree.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class ChangeTree : MonoBehaviour
 {
-    [SerializeField] private Sprite[] sprites;
-
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject playerDetectPoint;
     private int bossesKilled = 0;
     private PersistantData PD;
+    private Animator anim;
 
     private void Awake() {
         PD = GameObject.Find("PersistantData").GetComponent<PersistantData>();
+        anim = GetComponent<Animator>();
     }
 
     void Start() {
@@ -18,10 +20,47 @@ public class ChangeTree : MonoBehaviour
             bossesKilled++;
         if(PD.area2BossKilled)
             bossesKilled++;
+        if(PD.finalBossKilled)
+            bossesKilled++;
 
-        if(bossesKilled == 1)
-            gameObject.GetComponent<SpriteRenderer>().sprite = sprites[1];
-        else if(bossesKilled == 2)
-            gameObject.GetComponent<SpriteRenderer>().sprite = sprites[2];
+        if(bossesKilled == 1){
+            if(!PD.firstTransitionPlayed){
+                anim.SetTrigger("0to2");
+                PD.firstTransitionPlayed = true;
+            }
+            else{
+                anim.SetTrigger("2idle");
+            }
+        }
+        else if(bossesKilled == 2){
+            if(!PD.secondTransitionPlayed){
+                anim.SetTrigger("2to3");
+                PD.secondTransitionPlayed = true;
+            }
+            else if(PD.portalOpenTransitionPlayed){
+                anim.SetTrigger("4idle");
+            }
+            else{
+                anim.SetTrigger("3idle");
+            }
+        }
+        else if(bossesKilled == 3){
+            if(!PD.fullyHealedTransitionPlayed){
+                anim.SetTrigger("4to3"); // TODO this doesn't have an animation
+                PD.fullyHealedTransitionPlayed = true;
+            }
+            else{
+                anim.SetTrigger("3idle");
+            }
+        }
+    }
+
+    private void Update() {
+        Vector3 diff = player.transform.position - playerDetectPoint.transform.position;
+        if(bossesKilled == 2 && !PD.portalOpenTransitionPlayed && diff.magnitude < 10f){
+            //TODO stop player movement
+            anim.SetTrigger("3to4");
+            PD.portalOpenTransitionPlayed = true;
+        }
     }
 }
--- Assets/Scripts/PersistantData.cs
+++ Assets/Scripts/PersistantData.cs
@@ -7,22 +7,30 @@ public class PersistantData : MonoBehaviour
     public static PersistantData PD;
 
     // Guardian Data
+    [Header("Guardian Data")] [Space]
     public int guardianMaxHealth = 10;
     public int guardianCurrentHealth = 10;
     public int orbCount = 0;
 
     // Boss Data
-    public bool area1BossKilled = true;     
-    public bool area2BossKilled = true;     
-     
-    void Awake()
-        {
-            if(PD != null){
-                Destroy(this.gameObject);
-            }
-            else
-                PD = this;
-            
+    [Header("Boss Data")] [Space]
+    public bool area1BossKilled = false;
+    public bool area2BossKilled = false;
+    public bool finalBossKilled = false;
+
+    // Hub Tree Data
+    [Header("Hub Tree Data")] [Space]
+    public bool firstTransitionPlayed = false;
+    public bool secondTransitionPlayed = false;
+    public bool portalOpenTransitionPlayed = false;
+    public bool fullyHealedTransitionPlayed = false;
+
+    void Awake(){
+        if(PD != null){
+            Destroy(this.gameObject);
+        }
+        else
+            PD = this;   
             DontDestroyOnLoad(this);
         }
 }
